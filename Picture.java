99ea1f6982c4a1e06f5bcfe5bac215f781a40fd0
Miguel Bayon (Toshiba) <miguel.bayon@iessanandres.com>
2015-10-09 10:57:48 +0200

    Añadido sol2 y cambio de color sol

--- Picture.java
+++ Picture.java
@@ -6,7 +6,7 @@
  *
  * This class was written as an early example for teaching Java with BlueJ.
  * 
- * @author  Michael Kšlling and David J. Barnes
+ * @author  Michael Kï¿½lling and David J. Barnes
  * @version 2011.07.31
  */
 public class Picture
@@ -15,6 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
+    private Circle sun2;
 
     /**
      * Constructor for objects of class Picture
@@ -49,11 +50,19 @@ public class Picture
         roof.makeVisible();
 
         sun = new Circle();
-        sun.changeColor("yellow");
+        sun.changeColor("blue");
         sun.moveHorizontal(100);
         sun.moveVertical(-40);
         sun.changeSize(80);
         sun.makeVisible();
+        
+        
+        sun2 = new Circle();
+        sun2.changeColor("green");
+        sun2.moveHorizontal(50);
+        sun2.makeVisible();
+        
+        
     }
 
     /**
